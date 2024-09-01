b169d51003c4a35999676d8ceaa9a16f3022dd23
vipin-v <vipindas233@gmail.com>
2024-09-01 22:50:59 +0530

    Two sum

--- twoSum.dart
+++ twoSum.dart
@@ -1,15 +1,15 @@
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
-  Map<int, int> map = {};
+    Map<int, int> map = {};
 
-  for (int i = 0; i < nums.length; i++) {
-    int complement = target - nums[i];
+    for (int i = 0; i < nums.length; i++) {
+      int complement = target - nums[i];
 
-    if (map.containsKey(complement)) {
-      return [map[complement]!, i];
+      if (map.containsKey(complement)) {
+        return [map[complement]!, i];
+      }
+      map[nums[i]] = i;
     }
-    map[nums[i]] = i;
+    return [];
   }
-  return [];
-}
 }
